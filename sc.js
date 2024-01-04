0246b280844a89410f95317c7a843775d4714c8e
Pallavi Goli <pallavigoli11@gmail.com>
2024-01-04 17:59:38 +0530

    update

--- sc.js
+++
@@ -1,7 +0,0 @@
-function bulb(num) {
-    if (num==1) {
-        document.getElementById("center").src = "images/pic_bulbon.webp";
-    } else {
-        document.getElementById("center").src = "images/pic_bulboff.gif";
-    }
-}
